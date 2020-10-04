09ba5576eea52199976c20456c3b5da2b297020e
dimasta <sqzonow@mail.ru>
2020-10-04 19:58:22 +0500

    Updates

---
+++ precache-manifest.ad5b20e9d0cf896c2419648d184e33ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8996ed347c217f2a22d5743575d11ab2",
+    "url": "/finance-app/index.html"
+  },
+  {
+    "revision": "657091812dd144325cc1",
+    "url": "/finance-app/static/css/main.05a864e6.chunk.css"
+  },
+  {
+    "revision": "5541230e41994b1e0b03",
+    "url": "/finance-app/static/js/2.5f081e69.chunk.js"
+  },
+  {
+    "revision": "4bebe0170bbacb13c81f68e95cc2232e",
+    "url": "/finance-app/static/js/2.5f081e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657091812dd144325cc1",
+    "url": "/finance-app/static/js/main.431388e7.chunk.js"
+  },
+  {
+    "revision": "3d48b89ffb330a17f121",
+    "url": "/finance-app/static/js/runtime-main.ab478a5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/finance-app/precache-manifest.6211df1a1527a3f4f23e953d4e41e677.js"
+  "/finance-app/precache-manifest.ad5b20e9d0cf896c2419648d184e33ae.js"
 );
 
 self.addEventListener('message', (event) => {
